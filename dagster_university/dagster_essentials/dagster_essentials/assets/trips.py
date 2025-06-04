88cfc4f1182b33cff6b0044d1826e93c6388d8f3
dbtsnow2025-coder <snowzodiac@proton.me>
2025-06-04 13:16:12 -0400

    Update trips.py

--- dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
+++ dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
@@ -14,3 +14,15 @@ def taxi_trips_file() -> None:
 
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
+
+@dg.asset
+def taxi_zones_file () -> None:
+    """
+      The raw csv files for the taxi zones dataset.
+    """
+    taxi_zones = requests.get(
+        f"https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH.format(taxi_zones), "wb") as output_file:
+        output_file.write(taxi_zones.content)
