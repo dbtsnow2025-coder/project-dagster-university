86ac016197ceeb67766668624f1576fe14ba84cf
dbtsnow2025-coder <snowzodiac@proton.me>
2025-06-06 00:12:35 -0400

    Update resources.py

--- dagster_university/dagster_essentials/dagster_essentials/resources.py
+++ dagster_university/dagster_essentials/dagster_essentials/resources.py
@@ -1,5 +1,6 @@
 from dagster_duckdb import DuckDBResource
+import dagster as dg
 
 database_resource = DuckDBResource(
-    database="data/staging/data.duckdb"
+    database=dg.EnvVar("DUCKDB_DATABASE")      # replaced with environment variable
 )
